3d5a31335de1734170a3c39e840e44936fe50acd
Jason Karlin <jkarlin929@gmail.com>
2018-12-03 00:18:47 -0500

    Update stringly.js

--- stringly.js
+++ stringly.js
@@ -1,33 +1,11 @@
 function toUpper() {
-<<<<<<< HEAD
   console.log('I am on toUpper');
-=======
-<<<<<<< HEAD
-  console.log('I am on toUpper');
-=======
-  console.log('Hello Amanda');
->>>>>>> a66802f0115407d3216169a0b142aff130a63459
->>>>>>> 0556e39a3d3b65622f8b1a552b8d4f887a5478cf
 }
 
 toUpper();
 
 function toLower() {
-<<<<<<< HEAD
   console.log('I am on toLower');
 }
 
 toLower();
-=======
-<<<<<<< HEAD
-  console.log('I am on toLower');
-}
-
-toLower();
-=======
-  console.log('Hello Juan');
-}
-
-toLower()
->>>>>>> a66802f0115407d3216169a0b142aff130a63459
->>>>>>> 0556e39a3d3b65622f8b1a552b8d4f887a5478cf
